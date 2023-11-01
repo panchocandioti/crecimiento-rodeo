29d5308017c76c14565dcdc1f86bd83e9b10a997
panchocandioti <panchocandioti@gmail.com>
2023-10-31 21:28:54 -0300

    con footer

--- crecimiento-rodeo/src/Componentes/Presentacion.js
+++ crecimiento-rodeo/src/Componentes/Presentacion.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import IngresoDatos from './IngresoDatos';
 import MiLecheria from "../Media/MiLecheria.jpg"
+import LogoSaltoAgro from "../Media/LogoSaltoAgro.png"
+import LogoML1 from "../Media/LogoML1.png"
 
 function Presentacion() {
 
@@ -21,14 +23,13 @@ function Presentacion() {
                 <h1>CRECIMIENTO DEL RODEO LECHERO</h1>
             </header>
             {mostrarInstrucciones && (<div className="seccion">
-                <h2>Instrucciones:</h2>
+                <h2>Introducción:</h2>
                 <div id="instrucciones">
-                    <p>Durante un período de tiempo determinado (normalmente se toma un año) se considera que un rodeo lechero crece cuando el número de vacas adultas que finalizan su vida útil (abandonan el rodeo por muerte y/o por rechazo) es menor que el número de vacas nuevas (vaquillonas de reemplazo) que inician su vida productiva, verificándose que al final del período el rodeo de vacas adultas tiene más cabezas que al comienzo.</p>
-                    <p>Lo anterior debería lograrse como resultado de la dinámica propia del rodeo (crecimiento genuino o crecimiento interno del rodeo) sin necesidad de recurrir a la compra de vacas y/o vaquillonas. El crecimiento genuino es una estrategia de efecto consistente, sostenido e infinito en el tiempo que depende de la buena planificación y ejecución eficiente de los manejos del rodeo.</p>
-                    <p>Mediante el uso de esta aplicación web Usted podrá estimar la tasa anual de crecimiento genuino de un rodeo lechero a partir del ingreso de algunos parámetros de rodeo.</p>
-                    <p>Partiendo de un rodeo base Usted podrá evaluar los impactos en el crecimiento de rodeo que produce cada parámetro. La aplicación le permitirá evaluar tres sets de parámetros (Estados) en forma comparada. Por ejemplo, situación real versus otras dos situaciones alternativas.</p>
-                    <p>Los requerimientos de ingreso de parámetros están divididos en dos formularios: "Cálculo de bajas anuales y vida útil de vacas adultas" y "Cálculo de reposición anual efectiva". Debe completarse el primero para acceder al segundo. Cada uno de ellos presenta los resultados parciales al pie al finalizar el llenado.</p>
-                    <p>Haciendo click sobre la etiqueta de cada parámetro a ingresar se muestra una definición simplificada o un breve mensaje de ayuda.</p>
+                    <p>Durante un ejercio anual un rodeo lechero crece cuando las vacas adultas que finalizan su vida útil (muerte y/o por rechazo) son menos que las vacas nuevas (vaquillonas de reemplazo) que inician su vida productiva, finalizando el período con más vacas adultas que al comienzo.
+                        Esto debería lograrse como resultado de la dinámica propia del rodeo (crecimiento genuino) sin recurrir a compra de vacas y/o vaquillonas.</p>
+                    <p>Mediante el uso de esta aplicación web se puede estimar la tasa anual de crecimiento genuino de un rodeo lechero ingresando ciertos parámetros.</p>
+                    <p>El ingreso de parámetros está dividido en dos formularios: "Cálculo de bajas anuales y vida útil de vacas adultas" y "Cálculo de reposición anual efectiva". Debe completarse el primero para acceder al segundo.
+                        Para cada parámetro puede consultarse una definición simplificada o un breve mensaje de ayuda. Los valores ingresados pueden cambiarse en cualquier momento.</p>
                     <p>Resultados que se obtienen:</p>
                     <ul>
                         <li>Gráfico comparando bajas con reposición de corto y largo plazo</li>
@@ -36,15 +37,23 @@ function Presentacion() {
                         <li>Crecimiento porcentual del rodeo (corto y largo plazo)</li>
                         <li>Gráfico de evolución comparada a cinco años (para tres estados diferentes del rodeo y partiendo del número de vacas elegido por el usuario)</li>
                     </ul>
-                    <p>En el gráfico de evolución comparada los tres estados toman por defecto los valores de la primera simulación finalizada. A partir de allí se pueden hacer todos los cambios que el usuario desee. Cualquier situación simulada puede guardarse en cualquiera de los estados y ver su curva de evolución en el gráfico.</p>
-                    <p>Una vez guardadas las simulaciones en los tres estados el usuario podrá generar un reporte en formato PDF ingresando algunos datos adicionales incluido un email válido al que será enviado dicho reporte.</p>
-                
-                
+                    <p>En este último gráfico los tres estados toman por defecto los valores de la primera simulación terminada. Luego de hacer los cambios deseados cualquier situación simulada puede guardarse en un estado y visualizar su curva de evolución.</p>
+                    <p>Se podrá generar un reporte en formato PDF ingresando datos adicionales incluido un email válido al que será enviado dicho reporte.</p>
+                    <p>Secuencia de trabajo sugerida:</p>
+                    <ol>
+                        <li>Diagnóstico (realidad actual) (Estado 1)</li>
+                        <li>Detección de puntos críticos</li>
+                        <li>¿Qué pasaría si...? (pruebas)</li>
+                        <li>Selección de dos alternativas exitosas (Estados 2 y 3)</li>
+                        <li>Emisión del reporte final</li>
+                    </ol>
+                    <p>Debido a la gran cantidad de variables involucradas en la selección de datos de entrada, la interpretación de resultados y las aplicaciones de esta herramienta en general, el usuario asume toda la responsabilidad por su uso.</p>
+
                 </div>
             </div>)}
             <div>
                 <button onClick={manejarOnClick1}>
-                    {mostrarInstrucciones === true ? "Ocultar instrucciones" : "Mostrar instrucciones"}
+                    {mostrarInstrucciones === true ? "Ocultar introducción" : "Mostrar introducción"}
                 </button>
                 {comenzar === false && (<button onClick={manejarOnclick2}>
                     Aceptar términos y comenzar
@@ -53,6 +62,17 @@ function Presentacion() {
             {comenzar && (<div>
                 <IngresoDatos />
             </div>)}
+            <footer className='footer'>
+                <div>
+                    <p><b>Desarrollador:</b></p>
+                    <p>Ing. Agr. EPL Francisco Candioti</p>
+                    <p>panchocandioti@gmail.com</p>
+                </div>
+                <div className='containerLogos'>
+                    <img src={LogoSaltoAgro} className='logo'></img>
+                    <img src={LogoML1} className='logo'></img>
+                </div>
+            </footer>
         </div>
     )
 }
